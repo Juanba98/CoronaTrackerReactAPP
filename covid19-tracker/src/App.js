7cb36c606620058d8d22fd8aec2bee3131326f4e
Juanba1998 <juanbau1998@gmail.com>
2020-07-26 20:30:37 +0200

    select the country

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -1,11 +1,74 @@
-import React from 'react';
-
+import React, {useState,useEffect} from 'react';
+import {FormControl,Select,MenuItem} from '@material-ui/core';
 import './App.css';
 //BEM naming convention
+//"https://disease.sh/v3/covid-19/countries"
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  //USEEFFECT =  Runs a pierce of code
+  // based on a given condition
+
+  useEffect(() => {
+    //The code inside here will run once
+    //When the component loads and not again
+    //async -> send a request, wait for it, do something with it
+
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=> response.json())
+      .then((data)=> {
+        const countries = data.map((country) => (
+          {
+            name: country.country,//United States
+            value :  country.countryInfo.iso2,// UK, USA, FR
+          }
+        ));
+
+        setCountries(countries)
+      }); 
+    
+    };
+
+    getCountriesData();
+  }, [])
+
+
+const onCountryChange = (event) =>{
+  const countryCode = event.target.value;
+  setCountry(countryCode);
+}
+
   return (
     <div className="app">
-      <h1>Lets build a Covid 19 Tracker</h1>
+      
+      <div className = "app_header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className = "app_dropdown">
+          <Select variant = "outlined"  onChange = {onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+
+            {/* Loop throgh all the countries
+            and show a drop downa
+            list of the options*/}
+
+            {
+              countries.map((country)=>(
+                  
+              <MenuItem value={country.value}> {country.name}</MenuItem>
+                  
+                ))
+            }
+                
+          </Select>
+
+
+        </FormControl>
+      </div>
+
+
+      
 
 
       {/*Header*/}
