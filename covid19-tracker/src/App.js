8088a4e39d982ed9bcc5cb88b3867d6d90b57462
Juanba1998 <juanbau1998@gmail.com>
2020-07-26 18:31:23 +0200

    materialUi

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -6,6 +6,21 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build a Covid 19 Tracker</h1>
+
+
+      {/*Header*/}
+      {/*Title + Select input dropdown field*/}
+      {/*InfoBoxs*/}
+      {/*InfoBoxs*/}
+      {/*InfoBoxs*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+
+
+
+      {/*Map*/}
+
     </div>
   );
 }
